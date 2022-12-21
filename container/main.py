2a72ab849cec6485f8faf90ea88c751dd9ab591b
Matthew Henderson <info@matthewahenderson.com>
2022-12-21 18:08:59 -0500

    Added logic for getting API conn and started on constructor

---
+++ container/main.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from google.cloud import language_v1
+import spacy
+from spacy.matcher import Matcher
+from google.oauth2 import service_account
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import grequests
+
+class SpotifyTool:
+    def __init__(self) -> None:
+        try:
+            self.api_conn = self.get_api()
+            self.endpoint_url = 'https://api.spotify.com'
+        except:
+            print("Could not initialize Spotify tool class - check API creds")
+
+    def get_api(self):
+
+        redirect_uri = 'tbd'
+        access_id = os.environ.get('SPOTIFY_ID')
+        access_secret = os.environ.get('SPOTIFY_SECRET')
+        auth = SpotifyOAuth(
+            client_id = access_id,
+            client_secret = access_secret,
+            redirect_uri = redirect_uri
+        )
+
+        api = spotipy.Spotify(auth)
+        return api
+
+    def fetch_songs(self):
+        # Logic for pulling songs from Spotify
+        pass
+
+    
