5c296558fcd45dc0c5ac279b979260a49f770299
Matthew Henderson <info@matthewahenderson.com>
2023-02-19 17:48:04 -0500

    Implemented auth flow using spotipy

--- container/main.py
+++ container/main.py
@@ -1,50 +1,34 @@
 import os
 from google.oauth2 import service_account
-import requests
-import base64
+import spotipy
+import spotipy.util as util
 
 class SpotifyTool:
     def __init__(self) -> None:
         try:
-            self.api_conn = self.get_api()
-            self.endpoint_url = 'https://api.spotify.com'
+            self.api_conn = self.get_api_conn()
         except:
             print("Could not initialize Spotify tool class - check API creds")
 
-    def get_api(self):
+    def get_api_conn(self):
 
-        access_id = os.getenv('SPOTIFY_CLIENT_ID')
-        access_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
-        redirect_uri = os.getenv('SPOTIFY_REDIRECT_URI')
-        scopes = "user-read-private user-read-email"
-        
+        # Get auth token from env
         try:
-            auth_url = "https://accounts.spotify.com/api/token"
-            headers = {}
-            data = {}
-
-            message = f"{access_id}:{access_secret}"
-            messageBytes = message.encode('ascii')
-            base64Bytes = base64.b64encode(messageBytes)
-            base64Message = base64Bytes.decode('ascii')
-
-            headers['Authorization'] = f"Basic {base64Message}"
-            data['grant_type'] = "client_credentials"
-
-            r = requests.post(url=auth_url, headers=headers, data=data)
+            SPOTIPY_TOKEN = os.getenv('SPOTIPY_TOKEN')
+            api_conn = spotipy.Spotify(auth=SPOTIPY_TOKEN)
         except:
-            print("Could not get bearer token")
+            print('Could not get API token for auth flow - refreshing token...')
+            # logic for refresh?
         
-        
-        # return api
+        return api_conn
 
     def fetch_data(self):
-        # Logic for pulling listening datafrom Spotify
+        
+        # Logic for pulling listening data from Spotify
         results = self.api_conn.current_user_saved_tracks()
         for idx, item in enumerate(results['items']):
             track = item['track']
             print(idx, track['artists'][0]['name'], " – ", track['name'])
-        # pass
 
     def process_data(self):
         # Logic for formatting listening data
@@ -56,6 +40,4 @@ class SpotifyTool:
 
 if __name__ == "__main__":
     spotify = SpotifyTool()
-    spotify.get_api()
-
-
+    spotify.fetch_data()
